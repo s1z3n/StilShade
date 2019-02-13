78a26452e80a9d617bf979ed264cde3d2daa5cb8
Ali Beheshti <alibeheshti@triaset.com>
2019-02-13 17:37:05 +0330

    Customize roller shade

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,8 +1,60 @@
 $(document).ready(function() {
 
-  // $('.list2').hover(function(){
-  //   	$('.text1, .img1').addClass('is-open');
-  //
-  //   });
+  $('#swatch1').click(function(){
+    $('#map').css('background-color','#ebebf0');
+
+  });
+  $('#swatch2').click(function(){
+    $('#map').css('background-color','#edeeed');
+
+  });
+
+  $('#swatch3').click(function(){
+    $('#map').css('background-color','#e6e1db');
+
+  });
+
+  $('#swatch4').click(function(){
+    $('#map').css('background-color','#92a2af');
+
+  });
+
+  $('#swatch5').click(function(){
+    $('#map').css('background-color','#a9a59e');
+
+  });
+
+  $('#swatch6').click(function(){
+    $('#map').css('background-color','#a4a7a8');
+
+  });
+  $('#swatch7').click(function(){
+    $('#map').css('background-color','#9d9897');
+
+  });
+
+  $('#swatch8').click(function(){
+    $('#map').css('background-color','#6a6f77');
+
+  });
+  $('#swatch9').click(function(){
+    $('#map').css('background-color','#6b6a67');
+
+  });
+
+  $('#swatch10').click(function(){
+    $('#map').css('background-color','#6b615a');
+
+  });
+  $('#swatch11').click(function(){
+    $('#map').css('background-color','#2f3143');
+
+  });
+
+  $('#swatch12').click(function(){
+    $('#map').css('background-color','#0e0e11');
+
+  });
+
 
 });
